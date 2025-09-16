85c9b4404dda8559c46ba144e167832529888673
Antoni Poe Cruzado Pacheco <85760695+StrangeKode@users.noreply.github.com>
2025-09-15 22:01:51 -0500

    Add files via upload

---
+++ profile/profiledef.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="archcraft"
+iso_label="ARCHCRAFT_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Aditya Shakya <http://www.github.com/adi1090x>"
+iso_application="Archcraft Live/Installation/Rescue CD"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
+           'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+# Fix xz error, `export XZ_OPT="--threads=2"`
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '512K' '-Xdict-size' '512K')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:0400"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
+  ["/root/customize_airootfs.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/xflock4"]="0:0:755"
+)
